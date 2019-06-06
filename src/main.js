a1cabe25e260dd8cb9c09ab112af0d193383afd3
fengmengyang <fengmengyang@jd.com>
2019-06-06 19:17:40 +0800

    'fix'

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+// import 'element-ui/lib/theme-default/index.css'
 
 Vue.config.productionTip = false;
 
